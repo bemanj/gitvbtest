7b9b0bf03220666d67016700ef4b1bc035c6fd30
Robert John Millano <bemanj87@gmail.com>
2018-01-12 19:14:58 +0800

    add button new employee

--- GitVbTest/Form1.Designer.vb
+++ GitVbTest/Form1.Designer.vb
@@ -22,9 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.button_CreateNewEmployee = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'button_CreateNewEmployee
+        '
+        Me.button_CreateNewEmployee.Location = New System.Drawing.Point(12, 226)
+        Me.button_CreateNewEmployee.Name = "button_CreateNewEmployee"
+        Me.button_CreateNewEmployee.Size = New System.Drawing.Size(118, 24)
+        Me.button_CreateNewEmployee.TabIndex = 0
+        Me.button_CreateNewEmployee.Text = "New Employee"
+        Me.button_CreateNewEmployee.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.button_CreateNewEmployee)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents button_CreateNewEmployee As System.Windows.Forms.Button
 
 End Class
